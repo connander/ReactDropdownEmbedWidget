c50f516221b617f800a5abe5e03496733ccef279
connander <connorpstark@gmail.com>
2020-01-21 21:41:12 -0600

    included working react code for widget itself

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import React from 'react';
+import './app.css';
 
-const App = ({ title }) =>
-  <div>{title}</div>;
-
-export default App;
+class Toggle extends React.Component {
+  constructor(props){
+  super(props); 
+  this.state = {isToggleOn: true};
+  
+  this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick() {
+      this.setState(state => ({
+          isToggleOn: !state.isToggleOn
+      }));
+  }
+  
+  render() {
+      return (
+          <div>
+          <button className="button1" onClick ={this.handleClick}>{this.state.isToggleOn ? 'Buy Now' : 'Close'}</button>
+          <div className="dropdown-content">
+        <embed className="dropdown-embed" allowfullscreen="false" src="https://mealfu-standalone.bubbleapps.io/version-test/widget-mock"/>
+          </div>
+          </div>
+      );
+  }
+  }
+  
+  
+  export default Toggle;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import Toggle from './App';
 
-const title = 'My Minimal React Webpack Babel Setup';
 
-ReactDOM.render(
-  <App title={title} />,
-  document.getElementById('app')
-);
+ReactDOM.render(<Toggle />, document.querySelector("#root"));
 
 module.hot.accept();
