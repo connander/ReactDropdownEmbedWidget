d6c89e2c96fa56519e7a75eb8dd22b4f3faa6b0c
connander <connorpstark@gmail.com>
2020-01-23 15:30:51 -0600

    added link prefetch to decrease load time of embed when called

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import styles from './app.css';
 
 class Toggle extends React.Component {
   constructor(props){
-  super(props); 
+  super(props);
   this.state = {isToggleOn: false};
   this.handleClick = this.handleClick.bind(this);
   }
   
-  handleClick() {
+
+ handleClick() {
       this.setState(state => ({
           isToggleOn: !state.isToggleOn
       }));
@@ -19,7 +20,7 @@ class Toggle extends React.Component {
           <div>
           <button className="button1" onClick ={this.handleClick}>{this.state.isToggleOn ? 'Close' : 'Buy Now'}</button>
           <div style={{display: this.state.isToggleOn ? 'block' : 'none' }} className="dropdown-content">
-        <embed className="dropdown-embed" allowfullscreen="false" src="https://mealfu-standalone.bubbleapps.io/version-test/widget-mock"/>
+        <embed className="dropdown-embed" allowFullScreen={false} src="https://mealfu-standalone.bubbleapps.io/version-test/widget-mock"/>
           </div>
           </div>
       );
