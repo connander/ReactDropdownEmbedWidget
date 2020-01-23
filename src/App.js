b706740d757df615811913c9377700a8b629ff75
connander <connorpstark@gmail.com>
2020-01-23 13:10:07 -0600

    changed embed src destination for some testing

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class Toggle extends React.Component {
           <div>
           <button className="button1" onClick ={this.handleClick}>{this.state.isToggleOn ? 'Close' : 'Buy Now'}</button>
           <div style={{display: this.state.isToggleOn ? 'block' : 'none' }} className="dropdown-content">
-        <embed className="dropdown-embed" allowfullscreen="false" src="connorpstark.com"/>
+        <embed className="dropdown-embed" allowfullscreen="false" src="spacejam.com"/>
           </div>
           </div>
       );
